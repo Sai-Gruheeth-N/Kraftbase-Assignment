d49a5aa3eb8a5cfdc81e20d591b4e092f8806b95
Gruheeth <gruheeth.288@gmail.com>
2023-06-05 01:12:25 +0530

    Finish card in today event view

--- lib/screens/events/today_tab_view.dart
+++ lib/screens/events/today_tab_view.dart
@@ -1,6 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
+import 'package:kraftbase/resources/app_colors.dart';
 
 class TodayTabView extends StatelessWidget {
   const TodayTabView({super.key});
@@ -14,6 +16,7 @@ class TodayTabView extends StatelessWidget {
       Color(0xFF4C42C2),
       Color(0xFF42C2C2),
     ];
+    List<int> enrollCount = [85, 62, 45, 85, 62];
     return Stack(
       children: [
         Container(
@@ -81,11 +84,14 @@ class TodayTabView extends StatelessWidget {
                     ),
                     child: Row(
                       children: [
-                        const Icon(Icons.filter_alt_outlined),
+                        const Icon(
+                          Icons.filter_alt_outlined,
+                          color: Color(0xFF0D1C2E),
+                        ),
                         Text(
                           'Filter',
                           style: GoogleFonts.readexPro(
-                            color: const Color(0xFF939393),
+                            color: const Color(0xFF212121),
                             fontWeight: FontWeight.w400,
                           ),
                         ),
@@ -108,12 +114,194 @@ class TodayTabView extends StatelessWidget {
                 width: 360,
                 child: Card(
                   color: cardColor[index],
+                  child: EventCard(
+                    enrollCount: enrollCount[index],
+                  ),
                 ),
               );
             },
           ),
         ),
       ],
+    );
+  }
+}
+
+class EventCard extends StatelessWidget {
+  final int enrollCount;
+  const EventCard({
+    Key? key,
+    required this.enrollCount,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                height: 44,
+                width: 44,
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(8.0),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Cryptocurrency',
+                      style: GoogleFonts.readexPro(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 14,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                    Text(
+                      '7:23 AM > 9:41 AM',
+                      style: GoogleFonts.readexPro(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12,
+                        color: AppColors.whiteColor,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.more_horiz,
+                color: AppColors.whiteColor,
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Text(
+              'Lorem ipsum dolor sit amet consectetur. In proin scelerisque rhoncus magna odio tellus habitant eleifend. Justo risus eget pretium turpis ',
+              style: GoogleFonts.readexPro(
+                fontWeight: FontWeight.w400,
+                fontSize: 8.0,
+                height: 1.8,
+                color: AppColors.whiteColor,
+              ),
+            ),
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 60,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      child: Image.asset(
+                        'images/eventCard/participants/Participant0.png',
+                      ),
+                    ),
+                    Positioned(
+                      left: 12,
+                      child: Image.asset(
+                        'images/eventCard/participants/Participant1.png',
+                      ),
+                    ),
+                    Positioned(
+                      left: 24,
+                      child: Image.asset(
+                        'images/eventCard/participants/Participant2.png',
+                      ),
+                    ),
+                    Positioned(
+                      left: 36,
+                      child: Image.asset(
+                        'images/eventCard/participants/Participant3.png',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Participants',
+                style: GoogleFonts.readexPro(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12.0,
+                  color: AppColors.whiteColor,
+                ),
+              ),
+              const Spacer(),
+              const Icon(
+                Icons.signal_cellular_alt,
+                size: 15,
+                color: Color(0xFF37B622),
+              ),
+              Text(
+                'Live',
+                style: GoogleFonts.readexPro(
+                  fontWeight: FontWeight.w400,
+                  fontSize: 12.0,
+                  color: AppColors.whiteColor,
+                ),
+              ),
+            ],
+          ),
+          const Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(50.0),
+                  child: LinearProgressIndicator(
+                    value: enrollCount / 100,
+                    minHeight: 5,
+                    backgroundColor: const Color(0xFFC8C8C8),
+                    color: AppColors.whiteColor,
+                  ),
+                ),
+              ),
+              Text(
+                '$enrollCount%',
+                style: GoogleFonts.readexPro(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 10.0,
+                    color: AppColors.whiteColor),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color(0xFFFFD55D),
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 4.0,
+                    horizontal: 8.0,
+                  ),
+                  child: Row(
+                    children: [
+                      Text(
+                        'Enroll Now',
+                        style: GoogleFonts.readexPro(
+                          color: const Color(0xFF212121),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 10.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
